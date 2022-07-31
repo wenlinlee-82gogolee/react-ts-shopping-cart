5d3ac0b503ce3a9e130456511dfcc942975fc359
Wen-Lin Lee <82gogolee@gmail.com>
2022-07-31 19:25:22 +0800

    add the ShoppingCart component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,8 +6,11 @@ import {
   Badge,
 } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
+import { useShoppingCart } from '../context/ShoppingCartContext';
 
 export function Navbar() {
+  //from the context
+  const { openCart, cartQuantity } = useShoppingCart();
   return (
     <NavbarBs className="bg-white shadow-sm mb-3 sticky-top w-full">
       <Container>
@@ -22,29 +25,33 @@ export function Navbar() {
             About
           </Nav.Link>
         </Nav>
-        <Button
-          style={{ width: '3rem', height: '3rem', position: 'relative' }}
-          variant="outline-primary"
-          className="rounded-circle"
-        >
-          <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg">
-            <path d="M13.5 21c-.276 0-.5-.224-.5-.5s.224-.5.5-.5.5.224.5.5-.224.5-.5.5m0-2c-.828 0-1.5.672-1.5 1.5s.672 1.5 1.5 1.5 1.5-.672 1.5-1.5-.672-1.5-1.5-1.5m-6 2c-.276 0-.5-.224-.5-.5s.224-.5.5-.5.5.224.5.5-.224.5-.5.5m0-2c-.828 0-1.5.672-1.5 1.5s.672 1.5 1.5 1.5 1.5-.672 1.5-1.5-.672-1.5-1.5-1.5m16.5-16h-2.964l-3.642 15h-13.321l-4.073-13.003h19.522l.728-2.997h3.75v1zm-22.581 2.997l3.393 11.003h11.794l2.674-11.003h-17.861z" />
-          </svg>
-          <div
-            className="rounded-circle bg-danger d-flex justify-content-center align-items-center"
-            style={{
-              color: 'white',
-              position: 'absolute',
-              width: '1.3rem',
-              height: '1.3rem',
-              top: -5,
-              right: 0,
-              transform: 'translate(25%,25%)',
-            }}
+
+        {cartQuantity > 0 && (
+          <Button
+            onClick={openCart}
+            style={{ width: '3rem', height: '3rem', position: 'relative' }}
+            variant="outline-primary"
+            className="rounded-circle"
           >
-            3
-          </div>
-        </Button>
+            <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg">
+              <path d="M13.5 21c-.276 0-.5-.224-.5-.5s.224-.5.5-.5.5.224.5.5-.224.5-.5.5m0-2c-.828 0-1.5.672-1.5 1.5s.672 1.5 1.5 1.5 1.5-.672 1.5-1.5-.672-1.5-1.5-1.5m-6 2c-.276 0-.5-.224-.5-.5s.224-.5.5-.5.5.224.5.5-.224.5-.5.5m0-2c-.828 0-1.5.672-1.5 1.5s.672 1.5 1.5 1.5 1.5-.672 1.5-1.5-.672-1.5-1.5-1.5m16.5-16h-2.964l-3.642 15h-13.321l-4.073-13.003h19.522l.728-2.997h3.75v1zm-22.581 2.997l3.393 11.003h11.794l2.674-11.003h-17.861z" />
+            </svg>
+            <div
+              className="rounded-circle bg-danger d-flex justify-content-center align-items-center"
+              style={{
+                color: 'white',
+                position: 'absolute',
+                width: '1.3rem',
+                height: '1.3rem',
+                top: -5,
+                right: 0,
+                transform: 'translate(25%,25%)',
+              }}
+            >
+              {cartQuantity}
+            </div>
+          </Button>
+        )}
       </Container>
     </NavbarBs>
   );
--- src/context/ShoppingCartContext.tsx
+++ src/context/ShoppingCartContext.tsx
@@ -1,4 +1,5 @@
 import { createContext, useContext, ReactNode, useState } from 'react';
+import { ShoppingCart } from '../components/ShoppingCart';
 
 type ShoppingCartProviderProps = {
   children: ReactNode;
@@ -10,10 +11,14 @@ type CartItem = {
 };
 
 type ShoppingCartContext = {
+  openCart: () => void;
+  closeCart: () => void;
   getItemQuantity: (id: number) => number;
   increaseCartQuantity: (id: number) => void;
   decreaseCartQuantity: (id: number) => void;
   removeFromCart: (id: number) => void;
+  cartQuantity: number;
+  cartItems: CartItem[];
 };
 
 //as...means it will have type as ShoppingCartContext
@@ -24,7 +29,16 @@ export function useShoppingCart() {
 }
 
 export function ShoppingCartProvider({ children }: ShoppingCartProviderProps) {
+  const [isOpen, setIsOpen] = useState(false);
   const [cartItems, setCartItems] = useState<CartItem[]>([]);
+
+  const cartQuantity = cartItems.reduce(
+    (quantity, item) => item.quantity + quantity,
+    0
+  );
+
+  const openCart = () => setIsOpen(true);
+  const closeCart = () => setIsOpen(false);
 
   function getItemQuantity(id: number) {
     //?. means if we have that value, we retuem quamtity, or else zero
@@ -76,9 +90,14 @@ export function ShoppingCartProvider({ children }: ShoppingCartProviderProps) {
         increaseCartQuantity,
         decreaseCartQuantity,
         removeFromCart,
+        openCart,
+        closeCart,
+        cartItems,
+        cartQuantity,
       }}
     >
       {children}
+      <ShoppingCart isOpen={isOpen} />
     </ShoppingCartContext.Provider>
   );
 }
